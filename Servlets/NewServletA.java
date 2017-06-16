9cfcb8baf37907c04fe08e2bd540fa06d0e95ea4
Cox-Matt <matthew.d.cox3@gmail.com>
2017-06-16 15:48:44 -0500

    Add files via upload

---
+++ Servlets/NewServletA.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Matthew
+ */
+public class NewServletA extends HttpServlet {
+    
+    public String msg;
+    @Override
+    public void init(){
+        msg = "This shows the basics of implimenting servlets!";
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println(msg);
+        }catch(Exception e){
+        } finally {
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ Servlets/index.jsp
@@ -0,0 +1,31 @@
+<%-- 
+    Document   : index
+    Created on : Jun 15, 2017, 10:23:10 PM
+    Author     : Matthew
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action="NewServletA">
+            <h1>Basic Servlet</h1>
+            <br>
+            <input type="submit" value="For a Basic Servlet Press it!"/>
+        </form>
+        <form action="NewServletB">
+            <h2>Next Basic Servlet</h2>
+            <br>
+            <input type="submit" value="Push for a Yay Servlet!"/>
+        </form>
+        <form action="NewServletC">
+            <h3>Last Basic Servlet</h3>
+            <br>
+            <input type="submit" value="If you push this than your bored."/>
+        </form>
+    </body>
+</html>
