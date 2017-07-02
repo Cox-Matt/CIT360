af120645a37a981d3835739951f10b1407a0026f
Cox-Matt <matthew.d.cox3@gmail.com>
2017-07-01 20:15:13 -0600

    Add files via upload

---
+++ JUnit_Tests/assertTrue_assertFalse/tfprac.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Matthew
+ */
+public class tfprac {
+    public boolean compareString(String first,String second){
+        if(first.equalsIgnoreCase(second)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
